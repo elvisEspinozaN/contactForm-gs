d875487ddb3600be5c8a0fde5874a25df4ff6d61
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-06-08 11:06:58 -0400

    wip: done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-function transfer() {}
+function transfer() {
+  document.getElementById("fname-transfer").textContent = localStorage("fname");
+  document.getElementById("lname-transfer").textContent = localStorage("lname");
+  document.getElementById("email-transfer").textContent = localStorage("email");
+  document.getElementById("msg-transfer").textContent = localStorage("msg");
+}
