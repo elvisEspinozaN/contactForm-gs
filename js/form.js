6eeb9c89fa079066e2f859ee05f05d54571bee80
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-06-08 10:53:39 -0400

    wip: js for validation and transfer

--- js/form.js
+++ js/form.js
@@ -1,10 +1,21 @@
-function showtext() {
-  let fname = document.getElementById("fname").value;
-  let lname = document.getElementById("lname").value;
-  let email = document.getElementById("email").value;
-  let time = document.getElementById("time").value;
-  let msg = document.getElementById("msg").value;
+function validation() {
+  let email = document.getElementById("email");
+  let valid = true;
 
-  document.getElementById("show").innerHTML =
-    fname + "<br>" + lname + "<br>" + email + "<br>" + time + "<br>" + msg;
+  if (!email.value.toLowerCase().match("[a-z0-9]+@[a-z]+.[a-z]+")) {
+    email.style.background = "red";
+    valid = false;
+  }
+
+  if (valid) {
+    storeUserData();
+    window.location.href = "transfer.html";
+  }
+}
+
+function savedTrans() {
+  localStorage.setItem("fname", document.getElementById("fname").value);
+  localStorage.setItem("lname", document.getElementById("lname").value);
+  localStorage.setItem("email", document.getElementById("email").value);
+  localStorage.setItem("msg", document.getElementById("msg").value);
 }
